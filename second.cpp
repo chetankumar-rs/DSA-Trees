16c88f8c130d3157890a752a043cf7200539bd2e
Chetankumar R S <chetan5654c@gmail.com>
2024-12-30 23:41:31 +0530

    coded the inorder traversal

---
+++ second.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+struct NODE
+{
+    int data;
+    NODE* left;
+    NODE* right;
+
+    NODE(int value)
+    {   
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void inorder(NODE* value){
+    if(value == nullptr)
+        return;
+    else{
+            inorder(value->left);
+            cout<<value->data<<" ";
+            inorder(value->right);
+        }
+}
+
+int main(){
+    NODE* root = new NODE(1);
+    root->left = new NODE(2);
+    root->right = new NODE(3);
+    root->left->left = new NODE(4);
+    root->left->right  = new NODE(5);
+    root->right->left = new NODE(6);
+    root->right->right = new NODE(7);
+
+    cout<<"Inorder Traversal: ";
+    inorder(root);
+    return 0;
+}
